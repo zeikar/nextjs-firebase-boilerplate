8549341866f7cea963e42d93cf026735dde25efe
zeikar <zeikar@naver.com>
2025-03-30 20:06:18 +0900

    Add vercel web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 import { NotificationProvider } from "@/contexts/notification-context";
 
 const geistSans = Geist({
@@ -29,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <NotificationProvider>{children}</NotificationProvider>
+        <Analytics />
       </body>
     </html>
   );
